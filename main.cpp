96554db539f32dbbfadc68f44cc11ca47f70b231
minaaaaaa1010 <aminaasukhbaatar@gmail.com>
2025-04-10 07:43:18 +0800

    aminaa

--- main.cpp
+++ main.cpp
@@ -4,70 +4,78 @@
 using namespace std;
 
 // Shape class-г тодорхойлж байна
-class shape {
-    protected:
-        // Дүрсийн нэрийг хадгалах динамик массив
-        char *name;
-    
-        //Static khuvtsagch: bukh 'shape' bolon tuunii udam claasiin objectiin toog khadgalakh
-        static int object_count;
-    public:
-        //Ankhdagch baiguulagch(constructor)
-        shape() : name(nullptr) {
-            //Object-uusgekh burt object_count 1-eer nemegdene
-            this -> object_count++; // this ашиглаж object_count руу хандаж байна
-        }
-        // Ustagagch (destructor) funkts - objectiig sanakh oigoos ustgakh uyd duudagdana
-        virtual ~shape() {
-            //Khereb name ni nullptr bish bol sanakh oig chuluulnu
-            delete[] name; // name-г чөлөөлөх
-            this -> object_count--; // this ашиглаж object_count руу хандах
-    
-            //Object ustgakh burt object_count 1-eer bagsana
-            object_count--;
-        }
-    
-        //Utga onookh static funkts
-        static void set_count(int count) {
-            object_count = count;
-        }
-    
-        //Utga abakh static funkts
-        static int get_count() {
-            return object_count;
-        }
-    
-        //Odoogiin object-iin toog kheblekh
-        static void print_count() {
-            cout << "Niit object: " << object_count << endl;
-        }
-    
-        //Objectiin neriig butsaakh funkts
-        char *get_name() { 
-            return this->name; //this нь тухайн объектын name гишүүнд хандаж байна.
-        } 
-        //Objectiin neriig tikhiruulakh funkts
-        void set_name(const char *n) { 
-            //Khereb umnun ner ugsun bol sabakh oig chuluulnu
-            if (this -> name != nullptr) //this-ээр тухайн объектоос name гишүүн рүү шууд хандаж байнан.
-                delete[] this -> name;
+class shape
+{
+protected:
+    // Дүрсийн нэрийг хадгалах динамик массив
+    char *name;
 
-    
-            //Shine nert zoriulj sanakh oi gargakh, khuulakh
-            this -> name = new char[strlen(n) + 1]; // +1 ni tugsguliin '\0'-g zuriulagdsan
-            strcpy(this -> name, n); //temdegt muriig khuulna
-        }
-        virtual void print() { cout << name << " "; }
-    };
-    
-//Static khuvtsagchiig classiin gadna tald uusgej baina
-int shape::object_count = 0; //Ankhdagch utga 0 object baina
-    
+    // Static хувьсагч: бүх 'shape' болон түүнээс удамшсан классын объектийн тоог хадгалах
+    static int object_count;
 
+public:
+    // Анхдагч байгуулагч
+    shape() : name(nullptr)
+    {
+        // Объект үүсгэх үед object_count 1-ээр нэмэгдэнэ
+        this->object_count++; // this ашиглаж object_count руу хандаж байна
+    }
+    // Устгагч (destructor) функц - объектийг санах ойгоос устгах үед дуудагдана
+    virtual ~shape()
+    {
+        // Хэрэв name нь nullptr биш бол санах ойг чөлөөлнө
+        delete[] name;        // name-г чөлөөлөх
+        this->object_count--; // this ашиглаж object_count руу хандах
+
+        // Объект устгах бүрт object_count 1-ээр багасна
+        object_count--;
+    }
+
+    // Утга оноох статик функц
+    static void set_count(int count)
+    {
+        object_count = count;
+    }
+
+    // Утга авах статик функц
+    static int get_count()
+    {
+        return object_count;
+    }
+
+    // Одоогийн объектийн утгыг хэвлэх
+    static void print_count()
+    {
+        cout << "Niit object: " << object_count << endl;
+    }
+
+    // Объектийн нэрийг буцаах функц
+    char *get_name()
+    {
+        return this->name; // this нь тухайн объектын name гишүүнд хандаж байна.
+    }
+    // Объектийн нэрийг тохируулах функц
+    void set_name(const char *n)
+    {
+        // Хэрэв өмнө нь нэр өгсөн бол санах ойг чөлөөлнө
+        if (this->name != nullptr) // this-ээр тухайн объектоос name гишүүн рүү шууд хандана
+            delete[] this->name;   // Санах ойгоос чөлөөлнө
+
+        // Шинэ нэрт зориулж санах ой гаргах, хуулах
+        this->name = new char[strlen(n) + 1]; // +1 нь төгсгөлийн '\0'- д зориулагдсан
+        strcpy(this->name, n);                // тэмдэгт мөрийг хуулна
+    }
+    virtual void print() { cout << name << " "; }
+};
+
+// Static хувьсагчийг классын гадна талд үүсгэж байна
+int shape::object_count = 0; // Анхдагч утга оноож байна
+
+// Shape class-аас удамшсан twod class
 class twod : public shape
 {
 protected:
-    int a; //a
+    int a; // Талын урт эсвэл радиусыг хадгалах гишүүн өгөгдөл
 
 public:
     twod(int _a) : a(_a) {}        // анхдагч байгуулагч
@@ -75,75 +83,87 @@ public:
     virtual float perimeter() = 0; // периметр хадгалах жинхэнэ хийсвэр функц
     virtual void print()           // хэвлэх функц - нэр, хэмжээ, талбай, периметр
     {
-        shape::print();
+        shape::print(); // Shape class-н print-г дуудаж байна
         cout << a << " ";
-        cout << "talbai:" << this -> area() << endl;
-        cout << "perimeter:" << this -> perimeter() << endl; // виртуал функцүүдийг тухайн объектын төрлөөс хамаарч дуудуулж байна
+        // виртуал функцүүдийг тухайн объектын төрлөөс хамаарч дуудуулж байна
+        cout << "talbai:" << this->area() << endl;
+        cout << "perimeter:" << this->perimeter() << endl; 
     }
 };
 
-// Square class
-class circle : public twod {
-public:
-    circle(int _a) : twod(_a) { 
-        this -> set_name("Circle"); 
-    }
-
-    float area() override {
-        return M_PI * a * a; //duguin talbai
-    }
-    float perimeter() override
-    {
-        return M_PI * 2 * a;
-    }
-};
-
-// Square class 
-class square : public twod {
-public:
-    square(int _a) : twod(_a) {
-        this -> set_name("Square");
-    }
-    
-    float area() override {
-        return a * a;
-    }
-
-    float perimeter() override {
-        return 4 * a;
-    }
-};
-
-// Traingle class
-class triangle : public twod {
-public:
-    triangle(int _a) : twod(_a) {
-        this -> set_name("Triangle");
-    }
-
-    float area() override {
-        return (sqrt(3) / 4) * a * a;
-    }
-
-    float perimeter() override {
-        return 3 * a;
-    }
-};
-
-int main()
+// twod-гээс удамшсан Circle class
+class circle : public twod
 {
-    int size;
-    cout << "heden durs oruulah ve?: " << endl;
-    cin >> size;
-    
+public:
+    circle(int _a) : twod(_a)       // анхдагч байгуулагч
+    {
+        set_name("Circle");         // нэрийг Circle болгон тохируулна
+    }
+
+    float area() override           // area-г дахин тодорхойлно
+    {
+        return M_PI * a * a;        // дугуйн талбайг буцаана
+    }
+    float perimeter() override      // perimeter-г дахин тодорхойлно
+    {
+        return M_PI * 2 * a;        // дугуйн периметрийг буцаана
+    }
+};
+
+// twod-гээс удамшсан Square class
+class square : public twod
+{
+public:
+    square(int _a) : twod(_a)       // анхдагч байгуулагч
+    {
+        set_name("Square");         // нэриийг Square болгон тохируулна
+    }
+
+    float area() override           // area-г дахин тодорхойлно
+    {
+        return a * a;               // квадратын талбайг буцаана
+    }
+
+    float perimeter() override      // perimeter-г дахин тодорхойлно
+    {
+        return 4 * a;               // квадратын периметрийг буцаана
+    }
+};
+
+// twod-гээс удамшсан Traingle class
+class triangle : public twod        
+{
+public:
+    triangle(int _a) : twod(_a)     // анхдагч байгуулагч
+    {
+        set_name("Triangle");       // нэриийг Triangle болгон тохируулна
+    }
+
+    float area() override           // area-г дахин тодорхойлно
+    {
+        return (sqrt(3) / 4) * a * a; // зөв гурвалжингийн талбайг буцаана
+    }
+
+    float perimeter() override      // perimeter-г дахин тодорхойлно
+    {
+        return 3 * a;               // зөв гурвалжингийн периметрийг буцаана
+    }
+};
+
+int main() 
+{
+    int size; //
+    cout << "heden durs oruulah ve?: " << endl; 
+    cin >> size;//
+
     twod **shapes = new twod *[size]; // twod* руу заасан динамик массив зарлаж байна.
-    
+
     // Дүрсүүдийг оруулж буй давталт
     for (int i = 0; i < size; i++)
     {
         int k;
         int a;
-        cout << "durs " << i << ": " << endl
+        cout << "durs " << i+1 << ": " << endl
              << "1. Square" << endl
              << "2. Triangle" << endl
              << "3. Circle" << endl;
@@ -168,7 +188,7 @@ int main()
             break;
         }
     }
-    
+
     // Bubble sort ашиглан талбайгаар эрэмбэлнэ.
     for (int i = 0; i < size; i++)
     {
@@ -204,15 +224,17 @@ int main()
     }
     // Периметрээр эрэмбэлсэн дүрсүүдээ хэвлэнэ.
     cout << "Perimetereer erembelegdsen ni:" << endl;
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         shapes[i]->print();
         cout << endl;
     }
     // Санах ойгоос чөлөөлөх
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         delete shapes[i];
     }
     delete[] shapes;
-    
+
     return 0;
 }
