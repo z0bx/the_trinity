180090f4b55dbaba9404d131e355cdc707de5ed2
z0bx <temuulen.temka1106@gmail.com>
2025-04-13 17:02:18 +0800

    fix folder name

---
+++ lab9/employee.hpp
@@ -0,0 +1,6 @@
+#ifndef EMPLOYEE_HPP
+#define EMPLOYEE_HPP
+#include<iostream>
+#include<vector>
+using namespace std;
+#endif
---
+++ lab9/main.cpp
@@ -0,0 +1,4 @@
+#include "employee.hpp"
+int main(){
+    return 0;
+}
