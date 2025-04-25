8c093e703bf55622db5d7f176f3e3c96036d4387
z0bx <temuulen.temka1106@gmail.com>
2025-04-25 09:57:24 +0800

    comment changes

--- lab10/linkedlist.hpp
+++ lab10/linkedlist.hpp
@@ -4,27 +4,27 @@
 #include <iostream>
 using namespace std;
 
-// Node бүтэц нь LinkedList-ийн бүрэлдэхүүн хэсэг
+// Linked List-н node template structure
 template <class T>
 struct Node {
     T data; // Элементүүдийн өгөгдөл
-    Node* next; // Дараагийн элементийн холбоос
+    Node* next; // Дараагийн node-н холбоос
 
     Node(T t) : data(t), next(nullptr) {}
 };
 
-// LinkedList класс нь динамик өгөгдлийн бүтцийг ашиглах
+// LinkedList класс нь динамик өгөгдлийн бүтцийг ашиглана
 template <class T>
 class LinkedList {
 private:
-    Node<T>* head; // Жагсаалтын эхлэл
-    int size; // Жагсаалтын хэмжээ
+    Node<T>* head; // head pointer нь node төрлийн structure луу заана
+    int size; // LinkedList-н хэмжээ
 
 public:
-    // Конструктор
+    // Байгуулагч функц
     LinkedList() : head(nullptr), size(0) {}
 
-    // Деструктор, санах ойн менежмент
+    // Устгагч функц, бүх linkedlist-н node-үүдийг санах ойгоос чөлөөлнө
     ~LinkedList() {
         while (head != nullptr) {
             Node<T>* temp = head;
@@ -33,44 +33,54 @@ public:
         }
     }
 
-    // Элемент нэмэх функц
+    // Node нэмэх функц
     void add(T t) {
+        // new оператор ашиглан дараагийн node-г үүсгэнэ
         Node<T>* newNode = new Node<T>(t);
+        // Хэрэв head null-руу зааж байвал head шинэ node-рүү заана
         if (head == nullptr) {
             head = newNode;
-        } else {
+        }
+        // Үгүй бол linkedlist-н хамгийн ард нь дараагийн node-г үүсгэнэ 
+        else {
             Node<T>* temp = head;
             while (temp->next != nullptr) {
                 temp = temp->next;
             }
             temp->next = newNode;
         }
-        size++;
+        size++; // LinkedList-н хэмжээг нэмэгдүүлнэ
     }
 
-    // Определлэх индексээр элементийг оруулах
+    // Индексээр node-г оруулах
     void insert(T t, int index) {
+        // хэрэв index 0-с бага, эсвэл параметрт оруулсан index-н утга хэмжээгээс хэтэрвэл ажиллахгүй
         if (index < 0 || index > size) return;
-
+        // new оператор ашиглан дараагийн node-г үүсгэнэ
         Node<T>* newNode = new Node<T>(t);
+        // хэрэв index-н утга 0 байвал head шинэ node-рүү заана
         if (index == 0) {
             newNode->next = head;
             head = newNode;
-        } else {
+        } 
+        else {
+            // оруулах гэж байгаа index-г заасан node руу очоод шинэ node-рүү заана.
             Node<T>* temp = head;
             for (int i = 0; i < index - 1; ++i) {
                 temp = temp->next;
             }
+            // шинэ node нь дараагийн node-рүү заана
             newNode->next = temp->next;
             temp->next = newNode;
         }
-        size++;
+        size++; // linkedlist-н хэмжээг нэмэгдүүлнэ 
     }
 
     // Индексээр элементийг авах
     T get(int index) {
+        // параметрээр авсан утга хэмжээнээс хэтэрвэл утга авахгүй
         if (index < 0 || index >= size) throw out_of_range("Index out of range");
-
+        // node-рүү заасан заагч үүсгэн index хүртэл list-ээр гүйгээд утгыг буцаана
         Node<T>* temp = head;
         for (int i = 0; i < index; ++i) {
             temp = temp->next;
